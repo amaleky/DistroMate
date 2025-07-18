14229bb60aa752f2fed02faf2809ffd9f7540376
Alireza Maleky <maleky.dev@gmail.com>
2025-07-18 13:53:40 +0330

    feat: add remmina package

--- scripts/remote-desktop.sh
+++ scripts/remote-desktop.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
 main() {
-  if [ -n "$IS_WSL" ]; then
-    winget.exe install -e --id AnyDeskSoftwareGmbH.AnyDesk
-  else
-    case "$DETECTED_DISTRO" in
-    "debian")
-      BASE_URL="https://download.anydesk.com/linux/"
-      LATEST_DEB=$(wget -cO- $BASE_URL | grep -o 'href="[^"]*_amd64.deb"' | sed 's/href="//' | sed 's/"//' | head -1)
-      wget -cO /tmp/anydesk.deb "${BASE_URL}${LATEST_DEB}"
-      ensure_packages "/tmp/anydesk.deb"
-      rm -rfv /tmp/anydesk.deb
+  PROGRAMMING_OPTIONS=(
+    "AnyDesk" "Remmina"
+  )
+
+  select PROGRAMMING_CHOICE in "${PROGRAMMING_OPTIONS[@]}"; do
+    echo "Installing $PROGRAMMING_CHOICE..."
+    case "$PROGRAMMING_CHOICE" in
+    "AnyDesk")
+      if [ -n "$IS_WSL" ]; then
+        winget.exe install -e --id AnyDeskSoftwareGmbH.AnyDesk
+      else
+        case "$DETECTED_DISTRO" in
+        "debian")
+          BASE_URL="https://download.anydesk.com/linux/"
+          LATEST_DEB=$(wget -cO- $BASE_URL | grep -o 'href="[^"]*_amd64.deb"' | sed 's/href="//' | sed 's/"//' | head -1)
+          wget -cO /tmp/anydesk.deb "${BASE_URL}${LATEST_DEB}"
+          ensure_packages "/tmp/anydesk.deb"
+          rm -rfv /tmp/anydesk.deb
+          ;;
+        "arch")
+          ensure_packages "anydesk-bin"
+          ;;
+        "fedora")
+          BASE_URL="https://download.anydesk.com/linux/"
+          LATEST_RPM=$(wget -cO- $BASE_URL | grep -o 'href="[^"]*x86_64.rpm"' | sed 's/href="//' | sed 's/"//' | head -1)
+          wget -cO /tmp/anydesk.rpm "${BASE_URL}${LATEST_RPM}"
+          ensure_packages "/tmp/anydesk.rpm"
+          rm -rfv /tmp/anydesk.rpm
+          ;;
+        "mac")
+          ensure_packages "anydesk" "--cask"
+          ;;
+        esac
+        sudo systemctl disable anydesk
+      fi
       ;;
-    "arch")
-      ensure_packages "anydesk-bin"
-      ;;
-    "fedora")
-      BASE_URL="https://download.anydesk.com/linux/"
-      LATEST_RPM=$(wget -cO- $BASE_URL | grep -o 'href="[^"]*x86_64.rpm"' | sed 's/href="//' | sed 's/"//' | head -1)
-      wget -cO /tmp/anydesk.rpm "${BASE_URL}${LATEST_RPM}"
-      ensure_packages "/tmp/anydesk.rpm"
-      rm -rfv /tmp/anydesk.rpm
-      ;;
-    "mac")
-      ensure_packages "anydesk" "--cask"
+    "Remmina")
+      case "$DETECTED_DISTRO" in
+      "debian" | "arch" | "fedora")
+        ensure_packages "remmina"
+        ;;
+      esac
       ;;
     esac
-    sudo systemctl disable anydesk
-  fi
+    menu
+  done
 }
+
 
 main "$@"
