d1bc25e424254d2d88624132c60f149f523fa019
Alireza Maleky <maleky.dev@gmail.com>
2025-10-30 21:23:50 +0330

    feat: add spotify support

--- scripts/player.sh
+++ scripts/player.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
 main() {
-  if [ "$IS_WSL" == "true" ]; then
-    winget.exe install -e --id CodecGuide.K-LiteCodecPack.Full
-  else
-    case "$DETECTED_DISTRO" in
-    "mac")
-      ensure_packages "iina" "--cask"
-      ;;
-    *)
-      ensure_packages "mpv"
-      ;;
+  PLAYER_OPTIONS=(
+    "Video Player" "Spotify"
+  )
+  select PLAYER_CHOICE in "${PLAYER_OPTIONS[@]}"; do
+    echo "Installing $PLAYER_CHOICE..."
+    case $PLAYER_CHOICE in
+      "Video Player")
+        if [ "$IS_WSL" == "true" ]; then
+          winget.exe install -e --id CodecGuide.K-LiteCodecPack.Full
+        else
+          case "$DETECTED_DISTRO" in
+          "mac")
+            ensure_packages "iina" "--cask"
+            ;;
+          *)
+            ensure_packages "mpv"
+            ;;
+          esac
+        fi
+        ;;
+      "Spotify")
+        if [ "$IS_WSL" == "true" ]; then
+          winget.exe install WhatsApp
+        else
+          case $DETECTED_DISTRO in
+            "debian" | "fedora")
+              sudo snap install spotify
+              ;;
+            "arch")
+              ensure_packages "spotify-launcher"
+              ;;
+            "mac")
+              brew install --cask spotify
+              ;;
+          esac
+        fi
+        ;;
     esac
-  fi
+    menu
+  done
 }
 
 main "$@"
