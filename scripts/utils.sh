2c1e0d0c18cd38534c953a82bdf8298a47cc2bef
Alireza Maleky <maleky.dev@gmail.com>
2025-11-02 00:13:43 +0330

    fix: simplify repository addition for Ubuntu by checking each component individually

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -92,8 +92,17 @@ detect_env() {
 package_manager() {
   case "$DETECTED_DISTRO" in
   "debian")
-    if ! grep -q "universe" /etc/apt/sources.list.d/ubuntu.sources || ! grep -q "main" /etc/apt/sources.list.d/ubuntu.sources || ! grep -q "restricted" /etc/apt/sources.list.d/ubuntu.sources || ! grep -q "multiverse" /etc/apt/sources.list.d/ubuntu.sources; then
-      sudo add-apt-repository main universe restricted multiverse -y
+    if ! grep -q "main" /etc/apt/sources.list.d/ubuntu.sources; then
+      sudo add-apt-repository -y main
+    fi
+    if ! grep -q "universe" /etc/apt/sources.list.d/ubuntu.sources; then
+      sudo add-apt-repository -y universe
+    fi
+    if ! grep -q "restricted" /etc/apt/sources.list.d/ubuntu.sources; then
+      sudo add-apt-repository -y restricted
+    fi
+    if ! grep -q "multiverse" /etc/apt/sources.list.d/ubuntu.sources; then
+      sudo add-apt-repository -y multiverse
     fi
     ensure_packages "apt-transport-https ca-certificates gnupg-agent software-properties-common"
     ;;
