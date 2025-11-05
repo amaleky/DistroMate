782700e2ad146150807bf5b4defb91cfea68f9b2
Alireza Maleky <maleky.dev@gmail.com>
2025-11-05 23:05:59 +0330

    fix: improve drivers stability by switch to open-source

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 main() {
-  if [ "$IS_WSL" == "true" ]; then
+  if [ "$DETECTED_DISTRO" == "mac" ]; then
+    info "Driver installation is not supported on macOS."
+  elif [ "$IS_WSL" == "true" ]; then
     echo -e "\n NVIDIA: https://www.nvidia.com/en-us/software/nvidia-app/ \n"
     CPU_VENDOR=$(lscpu | grep 'Vendor ID' | awk '{print $3}')
     if [ "$CPU_VENDOR" == "GenuineIntel" ]; then
@@ -10,21 +12,30 @@ main() {
       echo -e "\n AMD: https://www.amd.com/en/support/download/drivers.html \n"
     fi
   else
-    INTEL_GPU=false
-    NVIDIA_GPU=false
-    if lspci | grep -i "Intel" > /dev/null; then
-      INTEL_GPU=true
+    IS_AMD=false
+    IS_INTEL=false
+    IS_NVIDIA=false
+    IS_RAZER=false
+    if lspci | grep -i "amd" > /dev/null; then
+      IS_AMD=true
     fi
-    if lspci | grep -i "NVIDIA" > /dev/null; then
-      NVIDIA_GPU=true
+    if lspci | grep -i "intel" > /dev/null; then
+      IS_INTEL=true
+    fi
+    if lspci | grep -i "nvidia" > /dev/null; then
+      IS_NVIDIA=true
+    fi
+    ensure_packages "usbutils"
+    if lsusb | grep -qi "Razer"; then
+      IS_RAZER=true
     fi
     case "$DETECTED_DISTRO" in
     "debian")
-      ensure_packages "fwupd ubuntu-drivers-common usbutils"
+      ensure_packages "fwupd ubuntu-drivers-common"
       if command -v ubuntu-drivers >/dev/null 2>&1; then
-        sudo ubuntu-drivers install
+        sudo ubuntu-drivers install --free-only
       fi
-      if lsusb | grep -qi "Razer"; then
+      if [ "$IS_RAZER" = true ]; then
         sudo add-apt-repository ppa:openrazer/stable
         sudo add-apt-repository ppa:polychromatic/stable
         sudo apt update
@@ -34,32 +45,32 @@ main() {
       fi
       ;;
     "arch")
-      ensure_packages "fwupd usbutils"
-      if [ "$NVIDIA_GPU" = true ]; then
-        ensure_packages "nvidia"
-        yay -Rcnssu --noconfirm xf86-video-nouveau vulkan-nouveau
+      ensure_packages "fwupd xorg-server xorg-xinit"
+      if [ "$IS_AMD" = true ]; then
+        ensure_packages "vulkan-radeon xf86-video-amdgpu xf86-video-ati"
       fi
-      if lsusb | grep -qi "Razer"; then
+      if [ "$IS_INTEL" = true ]; then
+        ensure_packages "intel-media-driver libva-intel-driver libva-mesa-driver mesa vulkan-intel"
+      fi
+      if [ "$IS_NVIDIA" = true ]; then
+        ensure_packages "vulkan-nouveau xf86-video-nouveau"
+        remove_packages "nvidia"
+      fi
+      if [ "$IS_RAZER" = true ]; then
         ensure_packages "linux-headers polychromatic openrazer-daemon"
         sudo gpasswd -a "$USER" plugdev
         sudo modprobe razerkbd
       fi
-      if command -v nvidia-inst >/dev/null 2>&1; then
-        nvidia-inst
-      fi
-      if command -v chwd >/dev/null 2>&1; then
-        sudo chwd -a
-      fi
       ;;
     "fedora")
-      ensure_packages "fwupd usbutils mesa-vulkan-drivers mesa-dri-drivers mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld libva libva-utils"
-      if [ "$INTEL_GPU" = true ]; then
+      ensure_packages "fwupd mesa-vulkan-drivers mesa-dri-drivers mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld libva libva-utils"
+      if [ "$IS_INTEL" = true ]; then
         ensure_packages "libva-intel-driver intel-media-driver intel-gpu-tools"
       fi
-      if [ "$NVIDIA_GPU" = true ]; then
+      if [ "$IS_NVIDIA" = true ]; then
         ensure_packages "nvidia-gpu-firmware akmod-nvidia xorg-x11-drv-nvidia-cuda libva-nvidia-driver"
       fi
-      if lsusb | grep -qi "Razer"; then
+      if [ "$IS_RAZER" = true ]; then
         sudo dnf config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
         sudo dnf config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
         ensure_packages "kernel-devel openrazer-meta polychromatic"
